1de4d23cdf53010029762ec945a818cb8f0b8bea
GuesswhoLW <cox6415.sol@gmail.com>
2024-07-24 20:43:40 +0200

    Add basic styling

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/styles.css"; // Import the CSS file
 
 createApp(App).use(store).use(router).mount("#app");
